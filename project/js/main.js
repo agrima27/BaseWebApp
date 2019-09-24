938ab608292d44f0578b8f6a23eeaaeaac1bc202
Agrima <agagarwa@iu.edu>
2019-09-24 13:33:45 +0530

    24thSeptChange

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,28 @@
+//$(document).ready(function(){
+ // getWeather();
+//})
+
+function getWeather (searchQuery) {
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&APPID="+apiKey;
+  
+//**Not able to clear out an error occured message 
+  $(".city").text("");
+  $(".temp").text("");
+  
+  $.ajax(url,{success: function(data){
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+  }, error: function(error){
+    $(".error-message").text("An error occured")
+  }})
+}
+
+function searchWeather() {
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
+}
+
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
